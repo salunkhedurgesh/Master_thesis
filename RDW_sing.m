abd0f5cd3fd260219253b9dcfbc0e32901505563
salunkhedurgesh <salunkhedurgesh@gmail.com>
2020-03-21 21:06:45 +0100

    Edited the objective function and added the RDW_sing.m in order to initiate the simplex with valid points (points that don't have a singularity in the RDW)

--- RDW_sing.m
+++ RDW_sing.m
@@ -1,12 +1,12 @@
-%h = 0; hk= 0; a = 1; a_prime = 1.25; t = 1;
-function c_qual = obj_func(a, a_prime, h, t)
+%Function to check if there lies a singularity in the Regular Dextrous
+%Workspace (RDW)
+function sing_bool = RDW_sing(a, a_prime, h, t)
     sphere_limit = pi/6;
     ball_halfwidth = sin(sphere_limit);
     hk =0; %For simplicity in the beginning, I am not involving hk
     i = 0; %iteration keeper
-    act_range = [t+h-0.5, t+h+0.5]; %Actuator stroke length, "t+h" is assumed to be the mid-position of the actuator
-    quality = 0; %initializing the value to calculate quality for each loop
-    c_qual = 0; %initializing the value to calculate cumulative quality
+    
+    sing_bool = 1; %initializing the value to 1 by assuming that there is no singularity
     for alpha = -1 : 0.01 :1
         for beta = -1 :0.01 :1
             %Singularity check
@@ -14,38 +14,9 @@ function c_qual = obj_func(a, a_prime, h, t)
             val_den = sqrt(2*cos(beta)*cos(alpha)*h*t-2*sin(beta)*cos(alpha)*a_prime*t-2*cos(beta)*a*a_prime-2*sin(beta)*a*h+2*sin(alpha)*hk*t+a^2+a_prime^2+h^2+hk^2+t^2)*sqrt(2*cos(beta)*sin(alpha)*a*h+2*cos(beta)*cos(alpha)*h*t-2*sin(beta)*sin(alpha)*a*hk-2*sin(beta)*cos(alpha)*hk*t+2*sin(alpha)*a_prime*t-2*cos(alpha)*a*a_prime+a^2+a_prime^2+h^2+hk^2+t^2);
             jac_det = abs(val/val_den);
             if jac_det < 0.01
-                c_qual = -inf;
+                sing_bool = 0;
                 break; %If any instance(alpha,beta) is singular, then the parameter is not useful
-            else
-                quality = 1;
             end   
-            
-            %Actuator lengths
-            rho1 = sqrt(2*cos(beta)*cos(alpha)*h*t-2*sin(beta)*cos(alpha)*a_prime*t-2*cos(beta)*a*a_prime-2*sin(beta)*a*h+2*sin(alpha)*hk*t+a^2+a_prime^2+h^2+hk^2+t^2);
-            rho2 = sqrt(2*cos(beta)*sin(alpha)*a*h+2*cos(beta)*cos(alpha)*h*t-2*sin(beta)*sin(alpha)*a*hk-2*sin(beta)*cos(alpha)*hk*t+2*sin(alpha)*a_prime*t-2*cos(alpha)*a*a_prime+a^2+a_prime^2+h^2+hk^2+t^2);
-
-            if rho1 < act_range(1) || rho1 > act_range(2)
-                quality = 0;
-                continue; %If any instance(alpha,beta) does not satisfy actuator length, then skip that iteration and evaluate the quality as '0'
-            elseif rho2 < act_range(1) || rho2 > act_range(2)
-                    quality = 0;
-                    continue;%If any instance (alpha,beta) does not satisfy actuator length, then skip that iteration and evaluate the quality as '0'
-            else
-                quality = 1;
-            end
-
-            %Passive limit - Universal joints
-            pass_lim_check_uni = p_lim_uni(a,a_prime, h, t, ball_halfwidth, alpha, beta);
-            if pass_lim_check_uni(1)*pass_lim_check_uni(2) == 0
-                quality = 0;%If any instance(alpha,beta) does not satisfy passive limits of the universal joint
-                            %, then skip that iteration and evaluate the quality as '0'
-                continue;
-            else
-                quality = 1;
-            end
-
-            c_qual = c_qual + quality;
         end
     end
-    c_qual = -c_qual; %This is necessary because I need to model an optimisation problem to minimize the objective function
 end
